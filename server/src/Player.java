3c80f7e09befcd911ffe7da7f07074c788b39840
Alexandros Laskos <a.laskos@campus.tu-berlin.de>
2021-05-07 01:30:15 +0200

    Update Player.java

--- server/src/Player.java
+++ server/src/Player.java
@@ -1,2 +1,30 @@
+import java.net.InetSocketAddress;
+
 public class Player {
+    private String name;
+    private String passwort;
+    private String email;
+    private int id;
+    private InetSocketAddress address;
+
+    public Player(String name, String passwort, String email, int id, InetSocketAddress address) {
+        this.name = name;
+        this.passwort = passwort;
+        this.email = email;
+        this.id = id;
+        this.address = address;
+    }
+
+    public String getName() { return this.name; }
+
+    public String getPasswort() { return this.passwort; }
+
+    public void setPasswort(String passwort) { this.passwort = passwort; }
+
+    public String getEmail() { return this.email; }
+
+    public int getId() { return this.id; }
+
+    public InetSocketAddress getAddress() { return this.address; }
+
 }
