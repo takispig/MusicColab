bc8334af57f6e212c53251de10748b2e78ae0085
MrGlas <akbigchris@gmail.com>
2021-06-19 13:11:12 +0200

    Reformatting InstrumentGUIBox and removing unused imports

--- MusicColabApp/app/src/main/java/com/example/musiccolab/instruments/InstrumentGUIBox.java
+++ MusicColabApp/app/src/main/java/com/example/musiccolab/instruments/InstrumentGUIBox.java
@@ -1,16 +1,13 @@
 package com.example.musiccolab.instruments;
 
-
-import android.animation.ObjectAnimator;
-import android.annotation.SuppressLint;
 import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.example.musiccolab.Lobby;
 import com.example.musiccolab.R;
 
@@ -39,7 +36,7 @@ public class InstrumentGUIBox {
         image4Theremin = (ImageView) lobby.findViewById(R.id.thereminView);
         defaultBlack = (ImageView) lobby.findViewById(R.id.blackView);
 
-        if(instrumentType==0){ //PIANO
+        if (instrumentType == 0) { //PIANO
             this.setDrumsInvisible();
             this.setThereminInvisible();
 
@@ -47,13 +44,13 @@ public class InstrumentGUIBox {
                 pianoKeys.add(lobby.findViewById(id));
             }
             setPianoKeysInvisible();
-        }else if(instrumentType==1){ //DRUMS
+        } else if (instrumentType == 1) { //DRUMS
             this.setPianoKeysInvisible();
             this.setThereminInvisible();
 
             image4Drums = lobby.findViewById(R.id.drumImageView);
             image4Drums.setVisibility(View.VISIBLE);
-        }else if(instrumentType==2){//THEREMIN
+        } else if (instrumentType == 2) {//THEREMIN
             this.setPianoKeysInvisible();
             this.setDrumsInvisible();
 
@@ -65,7 +62,6 @@ public class InstrumentGUIBox {
             image4Theremin.setVisibility(View.VISIBLE);
             defaultBlack.setVisibility(View.VISIBLE);
         }
-
     }
 
     public void setPianoKeysVisible() {
@@ -145,12 +141,7 @@ public class InstrumentGUIBox {
         defaultBlack.setImageAlpha(alphaScale);
     }
 
-
-
-
     public List<Button> getPianoKeys() {
         return pianoKeys;
     }
-
-
 }
