20d59872a438930670404f9d83a865894f0c98b0
name <a@b.c>
2021-06-19 16:58:55 +0200

    renamed piano files

--- MusicColabApp/app/src/main/java/com/example/musiccolab/instruments/SoundPlayer.java
+++ MusicColabApp/app/src/main/java/com/example/musiccolab/instruments/SoundPlayer.java
@@ -28,14 +28,14 @@ public class SoundPlayer {
 
     public void generateToneList() {
         sounds = new HashMap<>();
-        sounds.put("piano0", new MediaPlayerAdapter(lobby, R.raw.p_c, testingMode));
-        sounds.put("piano1", new MediaPlayerAdapter(lobby, R.raw.p_d, testingMode));
-        sounds.put("piano2", new MediaPlayerAdapter(lobby, R.raw.p_e, testingMode));
-        sounds.put("piano3", new MediaPlayerAdapter(lobby, R.raw.p_f, testingMode));
-        sounds.put("piano4", new MediaPlayerAdapter(lobby, R.raw.p_g, testingMode));
-        sounds.put("piano5", new MediaPlayerAdapter(lobby, R.raw.p_a, testingMode));
-        sounds.put("piano6", new MediaPlayerAdapter(lobby, R.raw.p_h, testingMode));
-        sounds.put("piano7", new MediaPlayerAdapter(lobby, R.raw.p_c2, testingMode));
+        sounds.put("piano0", new MediaPlayerAdapter(lobby, R.raw.piano0_c4, testingMode));
+        sounds.put("piano1", new MediaPlayerAdapter(lobby, R.raw.piano1_d4, testingMode));
+        sounds.put("piano2", new MediaPlayerAdapter(lobby, R.raw.piano2_e4, testingMode));
+        sounds.put("piano3", new MediaPlayerAdapter(lobby, R.raw.piano3_f4, testingMode));
+        sounds.put("piano4", new MediaPlayerAdapter(lobby, R.raw.piano4_g4, testingMode));
+        sounds.put("piano5", new MediaPlayerAdapter(lobby, R.raw.piano5_a4, testingMode));
+        sounds.put("piano6", new MediaPlayerAdapter(lobby, R.raw.piano6_h4, testingMode));
+        sounds.put("piano7", new MediaPlayerAdapter(lobby, R.raw.piano7_c5, testingMode));
         sounds.put("drums0", new MediaPlayerAdapter(lobby, R.raw.drum_a, testingMode));
         sounds.put("drums1", new MediaPlayerAdapter(lobby, R.raw.drum_b, testingMode));
         sounds.put("drums2", new MediaPlayerAdapter(lobby, R.raw.drum_c, testingMode));
